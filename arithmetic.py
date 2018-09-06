b87ecc650068117a3811fbdaac78b582b1ba9009
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-06 12:30:30 -0700

    add_mult and add_cubes functions working

--- arithmetic.py
+++ arithmetic.py
@@ -38,3 +38,15 @@ def power(num1, num2):
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
     return (num1 % num2)
+
+def add_mult(num1, num2, num3):
+    """Add first two numbers and multiply the sum with the third"""
+
+    return (multiply(add(num1, num2), num3))
+
+def add_cubes(num1, num2):
+    """cube both numbers and sum them"""
+
+    return (add(cube(num1), cube(num2)))
+
+
